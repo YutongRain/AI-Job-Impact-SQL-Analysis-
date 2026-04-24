342389c448db6475b203532cae7b2ce7b798a83c
Rain-yutong <yutongzhang_why@naver.com>
2026-04-24 15:45:13 +0200

    Create Data.sql

---
+++ Data.sql
@@ -0,0 +1,36 @@
+INSERT INTO jobs VALUES
+(1, 'Data Analyst', 'Technology', 85000, 'Bachelor'),
+(2, 'Truck Driver', 'Transportation', 45000, 'High School'),
+(3, 'Nurse', 'Healthcare', 70000, 'Bachelor'),
+(4, 'Accountant', 'Finance', 75000, 'Bachelor'),
+(5, 'Cashier', 'Retail', 28000, 'High School'),
+(6, 'Software Engineer', 'Technology', 120000, 'Bachelor'),
+(7, 'Teacher', 'Education', 55000, 'Master'),
+(8, 'Factory Worker', 'Manufacturing', 35000, 'High School'),
+(9, 'Tour Guide', 'Tourism', 40000, 'Bachelor'),
+(10, 'Hotel Receptionist', 'Tourism', 32000, 'High School'),
+(11, 'Travel Agent', 'Tourism', 38000, 'Bachelor'),
+(12, 'Adventure Guide', 'Tourism', 45000, 'Certificate');
+
+INSERT INTO ai_impact VALUES
+(1, 1, 0.45, 8.5, 2024),
+(2, 2, 0.89, 7.2, 2024),
+(3, 3, 0.15, 3.1, 2024),
+(4, 4, 0.72, 7.8, 2024),
+(5, 5, 0.91, 8.9, 2024),
+(6, 6, 0.30, 9.2, 2024),
+(7, 7, 0.20, 4.5, 2024),
+(8, 8, 0.95, 8.1, 2024),
+(9, 9, 0.20, 3.5, 2024),
+(10, 10, 0.75, 7.0, 2024),
+(11, 11, 0.80, 7.5, 2024),
+(12, 12, 0.10, 2.0, 2024);
+
+INSERT INTO industry_trends VALUES
+(1, 'Technology', 500000, 0.05, 2024),
+(2, 'Transportation', 120000, 0.35, 2024),
+(3, 'Healthcare', 80000, 0.08, 2024),
+(4, 'Finance', 200000, 0.25, 2024),
+(5, 'Retail', 90000, 0.40, 2024),
+(6, 'Manufacturing', 150000, 0.45, 2024),
+(7, 'Tourism', 50000, 0.15, 2024);
